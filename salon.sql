c57f7226339461927a7d18ce47440f0abfb55d2e
Seaside <n.peteros2003@gmail.com>
2024-11-28 03:49:46 +0800

    added updated database

--- salon.sql
+++ salon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2024 at 05:01 PM
+-- Generation Time: Nov 27, 2024 at 08:46 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `salon`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alternative_treatments`
+--
+
+CREATE TABLE `alternative_treatments` (
+  `id` int(11) NOT NULL,
+  `treatment_service_id` bigint(20) UNSIGNED NOT NULL,
+  `alternative_service_id` bigint(20) UNSIGNED NOT NULL,
+  `reason` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -41,6 +54,21 @@ CREATE TABLE `appointments` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `client_treatments`
+--
+
+CREATE TABLE `client_treatments` (
+  `id` int(11) NOT NULL,
+  `consultation_id` bigint(20) UNSIGNED NOT NULL,
+  `treatment_id` int(11) NOT NULL,
+  `status` enum('Suitable','Unsuitable') NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `consultations`
 --
 
@@ -50,14 +78,23 @@ CREATE TABLE `consultations` (
   `type` varchar(32) NOT NULL,
   `texture` varchar(32) NOT NULL,
   `hair` varchar(32) NOT NULL,
-  `perming` varchar(32) DEFAULT NULL,
-  `relax` varchar(32) DEFAULT NULL,
-  `rebonding` varchar(32) DEFAULT NULL,
-  `bleaching` varchar(32) DEFAULT NULL,
-  `removed` tinyint(1) NOT NULL,
+  `removed` tinyint(1) NOT NULL DEFAULT 0,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `previous_treatments`
+--
+
+CREATE TABLE `previous_treatments` (
+  `id` int(11) NOT NULL,
+  `treatment_id` int(11) NOT NULL,
+  `prev_treatment_id` int(11) NOT NULL,
+  `min_time_months` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -110,6 +147,17 @@ CREATE TABLE `stylist_specialties` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `treatments`
+--
+
+CREATE TABLE `treatments` (
+  `id` int(11) NOT NULL,
+  `service_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -125,8 +173,26 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `img_path`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin@salon.com', '8d66a53a381493bec08da23cef5a43767f20a42c', 'owner', 'default.png', '2024-10-25 20:51:08', '2024-10-25 20:51:08'),
+(2, 'Manager', 'manager@salon.com', '228a10e6b013b91cb98b428d4388af35f00fa6d4', 'manager', 'default.png', '2024-10-25 20:53:45', '2024-10-25 20:53:45'),
+(3, 'User', 'user@salon.com', 'c0018fdbfc43f406264c2a3d82cab7373ae090a1', 'user', 'default.png', '2024-11-10 04:36:46', '2024-11-10 04:36:46'),
+(4, 'Stylist', 'stylist@salon.com', 'f63bfa72406622a94c36df4595395cfe169c6b23', 'stylist', 'default.png', '2024-11-22 02:08:47', '2024-11-22 02:08:47');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `alternative_treatments`
+--
+ALTER TABLE `alternative_treatments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `treatment_service_id` (`treatment_service_id`),
+  ADD KEY `alternative_service_id` (`alternative_service_id`);
 
 --
 -- Indexes for table `appointments`
@@ -138,11 +204,27 @@ ALTER TABLE `appointments`
   ADD KEY `appointments_service_id_foreign` (`service_id`);
 
 --
+-- Indexes for table `client_treatments`
+--
+ALTER TABLE `client_treatments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `consultation_id` (`consultation_id`),
+  ADD KEY `treatment_id` (`treatment_id`);
+
+--
 -- Indexes for table `consultations`
 --
 ALTER TABLE `consultations`
   ADD PRIMARY KEY (`id`),
   ADD KEY `consultations_customer_id_foreign` (`customer_id`);
+
+--
+-- Indexes for table `previous_treatments`
+--
+ALTER TABLE `previous_treatments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `treatment_id` (`treatment_id`),
+  ADD KEY `prev_treatment_id` (`prev_treatment_id`);
 
 --
 -- Indexes for table `reviews`
@@ -168,6 +250,13 @@ ALTER TABLE `stylist_specialties`
   ADD KEY `stylist_specialties_service_id_foreign` (`service_id`);
 
 --
+-- Indexes for table `treatments`
+--
+ALTER TABLE `treatments`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `service_id` (`service_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -180,44 +269,75 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `alternative_treatments`
+--
+ALTER TABLE `alternative_treatments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `client_treatments`
+--
+ALTER TABLE `client_treatments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `consultations`
 --
 ALTER TABLE `consultations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `previous_treatments`
+--
+ALTER TABLE `previous_treatments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `stylist_specialties`
 --
 ALTER TABLE `stylist_specialties`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `treatments`
+--
+ALTER TABLE `treatments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1243;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `alternative_treatments`
+--
+ALTER TABLE `alternative_treatments`
+  ADD CONSTRAINT `alternative_treatments_ibfk_1` FOREIGN KEY (`treatment_service_id`) REFERENCES `services` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `alternative_treatments_ibfk_2` FOREIGN KEY (`alternative_service_id`) REFERENCES `services` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `appointments`
@@ -228,10 +348,24 @@ ALTER TABLE `appointments`
   ADD CONSTRAINT `appointments_stylist_id_foreign` FOREIGN KEY (`stylist_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `client_treatments`
+--
+ALTER TABLE `client_treatments`
+  ADD CONSTRAINT `client_treatments_ibfk_1` FOREIGN KEY (`consultation_id`) REFERENCES `consultations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `client_treatments_ibfk_2` FOREIGN KEY (`treatment_id`) REFERENCES `treatments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `consultations`
 --
 ALTER TABLE `consultations`
   ADD CONSTRAINT `consultations_customer_id_foreign` FOREIGN KEY (`customer_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `previous_treatments`
+--
+ALTER TABLE `previous_treatments`
+  ADD CONSTRAINT `previous_treatments_ibfk_1` FOREIGN KEY (`treatment_id`) REFERENCES `treatments` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `previous_treatments_ibfk_2` FOREIGN KEY (`prev_treatment_id`) REFERENCES `treatments` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `reviews`
@@ -246,6 +380,12 @@ ALTER TABLE `reviews`
 ALTER TABLE `stylist_specialties`
   ADD CONSTRAINT `stylist_specialties_service_id_foreign` FOREIGN KEY (`service_id`) REFERENCES `services` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `stylist_specialties_stylist_id_foreign` FOREIGN KEY (`stylist_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `treatments`
+--
+ALTER TABLE `treatments`
+  ADD CONSTRAINT `treatments_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `services` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
